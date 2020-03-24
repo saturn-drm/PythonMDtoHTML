a0ce47df94a21e85097ee946cd89f13f9c9c45ab
Zihan Mei <meizihanyx@gmail.com>
2020-03-24 01:48:44 -0400

    test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #%%
 from frontmatter import Frontmatter
 
-post = Frontmatter.read_file('../saturn-drmtest.github.io/2017-08-20-travel-is-meaningful.md')
+post = Frontmatter.read_file('../saturn-drmtest.github.io/posts/02tabs/2017-08-20-travel-is-meaningful.md')
 
 # %%
 post
@@ -13,5 +13,52 @@ post['body']
 import markdown
 htmlres = markdown.markdown(post['body'])
 htmlres
+
+# %%
+post2 = Frontmatter.read_file('../saturn-drmtest.github.io/posts/01blog/01digest/2020-01-23-放言五首.md')
+post2['body']
+
+# %%
+htmlres2 = markdown.markdown(post2['body'])
+htmlres2
+
+# %%
+outputFile = open('testoutput.html','w',encoding='utf-8',errors='xmlcharrefreplace')
+outputFile.write(htmlres2)
+
+# %%
+outputFile.close()
+
+# %%
+htmlwithtoc = markdown.markdown(post['body'], extensions=['toc'])
+htmlwithtoc
+
+# %%
+outputFile2 = open('testoutput-toc.html','w',encoding='utf-8',errors='xmlcharrefreplace')
+outputFile2.write(htmlwithtoc)
+outputFile2.close()
+outputFile3 = open('testoutput-notoc.html','w',encoding='utf-8',errors='xmlcharrefreplace')
+outputFile3.write(htmlwithtoc)
+outputFile3.close()
+
+# %%
+htmlwithtoc.toc
+
+# %%
+md = markdown.Markdown(extensions=['toc'])
+htmlwithtoc2 = md.convert(post['body'])
+htmlwithtoc2
+
+# %%
+md.toc
+
+# %%
+with open('testoutput-toc.html','r+',encoding='utf-8',errors='xmlcharrefreplace') as f:
+    old = f.read()
+    f.seek(0)
+    f.write(md.toc)
+    f.write(old)
+    f.close()
+
 
 # %%
